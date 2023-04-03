9fce03ce22ce6bb67976c887d852aaeb8194a1cc
JORDAAAN1495 <jordaaan1495@gmail.com>
2023-04-03 21:33:10 +0100

    Register via DI extension

---
+++ src/JK.Finances.DataAccess/DependencyInjection/DependencyInjectionExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace JK.Finances.DataAccess.DependencyInjection;
+
+public static class DependencyInjectionExtensions
+{
+    public static void AddDataAccess(this IServiceCollection services)
+    {
+        services.AddScoped<IDbConnection, DbConnection.DbConnection>();
+        services.AddScoped<IMongoAccountService, MongoAccountService>();
+    }
+}
--- src/JK.Finances.DataAccess/GlobalUsings.cs
+++ src/JK.Finances.DataAccess/GlobalUsings.cs
@@ -1,4 +1,6 @@
 ﻿global using JK.Finances.DataAccess.DbConnection;
+global using JK.Finances.DataAccess.Services.Account;
 global using JK.Finances.Infrastructure.Models.Account;
 global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
 global using MongoDB.Driver;
