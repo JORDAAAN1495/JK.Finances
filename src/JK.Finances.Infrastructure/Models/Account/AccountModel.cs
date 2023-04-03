45f0885e5b4e9c107ec2292d4fe5a3baf4e09899
JORDAAAN1495 <jordaaan1495@gmail.com>
2023-04-03 19:35:52 +0100

    Added Infra Models

---
+++ src/JK.Finances.Infrastructure/Models/Account/AccountModel.cs
@@ -0,0 +1,14 @@
+﻿namespace JK.Finances.Infrastructure.Models.Account;
+
+public class AccountModel
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; } = string.Empty;
+
+    public string Name { get; set; } = string.Empty;
+
+    public DateTime CreatedDateTime { get; set; } = DateTime.UtcNow;
+
+    public DateTime UpdatedDateTime { get; set; } = DateTime.UtcNow;
+}
