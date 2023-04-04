c60db99b3c2a8e5d90f3e75df7341ca838774667
JORDAAAN1495 <jordaaan1495@gmail.com>
2023-04-04 20:16:58 +0100

    Updated Build.bat > Build.ps1. Moved Mongo Connection string to User Secrets

---
+++ Build.ps1
@@ -0,0 +1,18 @@
+﻿$Host.Ui.RawUI.WindowTitle = $MyInvocation.MyCommand
+
+$SonarHost = [System.Environment]::GetEnvironmentVariable("SonarHost")
+$SonarLogin = [System.Environment]::GetEnvironmentVariable("JK.Finances.SonarLogin")
+$SonarProjectId = [System.Environment]::GetEnvironmentVariable("JK.Finances.SonarProjectId")
+$CoverageReport = [System.Environment]::GetEnvironmentVariable("JK.Finances.CoverageReport")
+
+dotnet tool install --global dotnet-sonarscanner
+
+dotnet sonarscanner begin /k:$SonarProjectId /d:sonar.host.url=$SonarHost /d:sonar.login=$SonarLogin /d:sonar.cs.vscoveragexml.reportsPaths=$CoverageReport
+
+dotnet build --no-incremental
+
+dotnet-coverage collect "dotnet test" -f xml  -o $CoverageReport
+
+dotnet sonarscanner end /d:sonar.login=$SonarLogin
+
+pause
